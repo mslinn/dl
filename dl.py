5236fd03ff35ccbe45def9dac8f438a6cd854e48
Mike Slinn <mslinn@mslinn.com>
2023-07-15 11:19:24 -0400

    -

--- dl.py
+++ dl.py
@@ -1,14 +1,19 @@
 import json
 import re
+import sys
 import yt_dlp
 
 format = 'mp3'
-URL = 'https://www.youtube.com/watch?v=BaW_jenozKc'
+if len(sys.argv)==1:
+    URL = 'https://www.youtube.com/watch?v=BaW_jenozKc'
+else:
+    URL = sys.argv[1]
 
 with yt_dlp.YoutubeDL({}) as ydl:
     info = ydl.extract_info(URL, download=False)
 
 name = re.sub(r'[^A-Za-z0-9 ]+', '', info['title']).strip().replace(' ', '_')
+name = re.sub('__+', '_', name)
 ydl_opts = {
     'format': f'{format}/bestaudio/best',
     # See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
