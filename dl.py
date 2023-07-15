c03cafe0d432b6a3af69781b168d67d572a9bb02
Mike Slinn <mslinn@mslinn.com>
2023-07-15 11:10:25 -0400

    -

---
+++ dl.py
@@ -0,0 +1,23 @@
+import json
+import re
+import yt_dlp
+
+URL = 'https://www.youtube.com/watch?v=BaW_jenozKc'
+
+with yt_dlp.YoutubeDL({}) as ydl:
+    info = ydl.extract_info(URL, download=False)
+
+name = re.sub(r'[^A-Za-z0-9 ]+', '', info['title']).strip().replace(' ', '_')
+ydl_opts = {
+    'format': 'mp4a/bestaudio/best',
+    # See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
+    'postprocessors': [{  # Extract audio using ffmpeg
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'm4a',
+    }],
+    'outtmpl': name
+}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    # print(json.dumps(ydl.sanitize_info(info)))
+    error_code = ydl.download(URL)
