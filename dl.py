b9b247f1f699e743ccc9adc9e0073f300795ef59
Mike Slinn <mslinn@mslinn.com>
2023-07-15 11:34:08 -0400

    -

--- dl.py
+++ dl.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
 
+import inspect
 import json
 import re
 import sys
 import yt_dlp
 
+def help(msg):
+    if len(msg.strip())>0:
+        print(msg + "\n")
+    print(inspect.cleandoc(f"""
+        {sys.argv[0]} - Download videos from various sites, including YouTube.
+        Can save video, or can just save audio as mp3 to $MP3_DEST on the local machine.
+        Also copies to gojira.
+
+        Usage: $( basename $0) [options] url
+
+        Options are:
+            -h       display this message and quit
+            -v       download video to $VDEST
+            -V dir   download video to any given directory
+            -x       download video to $XDEST
+
+        Last modified 2023-07-15
+        """))
+    exit(1)
+
+
 format = 'mp3'
 if len(sys.argv)==1:
-    print("Error: No URL was provided.")
+    help("Error: No URL was provided.")
     sys.exit(2)
 else:
     URL = sys.argv[1]
