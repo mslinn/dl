02f8df635f9d66c7a11a83f868e4bdd64d31c4eb
Mike Slinn <mslinn@mslinn.com>
2023-08-19 15:43:51 -0400

    -

--- src/dl/tests/test_dl_config.py
+++ src/dl/tests/test_dl_config.py
@@ -4,6 +4,7 @@ from dl import *
 class TestDLConfig:
     def test_new(self):
         config = DLConfig(config_path='dl.config')
+        assert False==True
         assert config.local
         assert config.remotes
         assert config.active_remotes
