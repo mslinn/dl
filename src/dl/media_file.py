c44cc99f139c7c0cafcd1f0b87bdcecc8cd0113f
Mike Slinn <mslinn@mslinn.com>
2023-08-19 19:51:17 -0400

    -

--- src/dl/media_file.py
+++ src/dl/media_file.py
@@ -11,21 +11,28 @@ class MediaFile:
         self.mp3_dest = self.set_mp3_dest()
 
     def is_mp3(self) -> bool:
-      return self.path.endswith('.mp3')
+      return self.path.name.endswith('.mp3')
 
     def is_video(self) -> bool:
-      return self.path.endswith('.mp4')
+      return self.path.name.endswith('.mp4')
 
+    # If mp3s directory specified in config file, use it
+    # Else if mp3s is defined as an environment variable, use it
+    # Else use ~/Music/mp3s if it exists
+    # Else crash exit
     def set_mp3_dest(self) -> None:
-        if self.config['mp3s'].isdir():
-            mp3_dest = self.config['mp3s']
-        elif environ.get('mp3s') is not None:
-            mp3_dest = environ.get('mp3s')
-            if not os.path.isdir(mp3_dest):
-                sys.exit(f"mp3s environment variable points to {mp3_dest}, but that directory does not exist.")
-        elif os.path.expandvars(os.path.isdir(self.config['mp3s'])):
-            mp3_dest = os.path.expandvars(self.config['mp3s'])
-        else:
-            mp3_dest = os.path.expanduser('~') + "/Music/mp3s"
-            if not os.path.isdir(mp3_dest):
-                sys.exit('Unable to find directory for mp3s.')
+        mp3s = self.config.mp3s(self.config.local)
+        if isinstance(mp3s, Path) and mp3s.is_dir():
+            self.mp3_dest = mp3s
+            return
+
+        mp3s = environ.get('mp3s')
+        if isinstance(mp3s, str) and len(str(mp3s))>0:
+            self.mp3_dest = Path(mp3s)
+            if not self.mp3_dest.is_dir():
+                sys.exit(f"mp3s environment variable points to '{self.mp3_dest}', but that directory does not exist.")
+            return
+
+        self.mp3_dest = os.path.expanduser('~') + "/Music/mp3s"
+        if not os.path.isdir(self.mp3_dest):
+            sys.exit('Unable to find directory for mp3s.')
