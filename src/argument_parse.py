037ae839d20f477e2c41aa9ac1add5f8f7713ab5
Mike Slinn <mslinn@mslinn.com>
2023-08-19 10:27:59 -0400

    -

--- src/argument_parse.py
+++ src/argument_parse.py
@@ -1,12 +1,13 @@
 from dl_config import DLConfig
-import inspect
-import os
-import textwrap
+from textwrap import fill, dedent
+from inspect import cleandoc
+from os.path import expandvars
 from argparse import ArgumentParser, HelpFormatter
 
+# See https://stackoverflow.com/a/64102901/553865
 class RawFormatter(HelpFormatter):
     def _fill_text(self, text, width, indent):
-        return "\n".join([textwrap.fill(line, width) for line in textwrap.indent(textwrap.dedent(text), indent).splitlines()])
+        return "\n".join([fill(line, width) for line in indent(dedent(text), indent).splitlines()])
 
 class ArgParse:
     def __init__(self, config: DLConfig) -> None:
@@ -14,19 +15,19 @@ class ArgParse:
         self.config = config
         self.format = 'mp3'
 
-        parser = self.make_arg_parser()
-        args = parser.parse_args()
+        self.parser = self.make_arg_parser()
+        self.args = self.parser.parse_args()
 
-        action = 'mp3'
-        format = 'mp3'
-        if args.keep_video or args.video_dest or args.xrated:
-            action = 'video'
-            format = 'mp4'
+        self.action = 'mp3'
+        self.format = 'mp3'
+        if self.args.keep_video or self.args.video_dest or self.args.xrated:
+            self.action = 'video'
+            self.format = 'mp4'
 
     def make_description(self):
         remotes = list(self.config.active_remotes())
         aremotes = ", ".join(list(map(lambda x: x, remotes)))
-        return inspect.cleandoc(f"""
+        return cleandoc(f"""
             Downloads media.
             Defaults to just downloading an MP3, even when the original is a video, unless the -x, -v or -V options are provided.
 
@@ -39,14 +40,14 @@ class ArgParse:
 
     def make_arg_parser(self):
         desc = self.make_description()
-        desc_expanded = os.path.expandvars(desc)
+        desc_expanded = expandvars(desc)
         parser = ArgumentParser(prog='dl',
                         description=desc_expanded,
-                        epilog=f"Last modified 2023-07-16.",
+                        epilog=f"Last modified 2023-08-18.",
                         formatter_class=RawFormatter)
         parser.add_argument('url')
         parser.add_argument('-d', '--debug', action='store_true', help="Enable debug mode")
-        parser.add_argument('-v', '--keep-video', action='store_true', help=os.path.expandvars(f"Download video to {self.config.local['vdest']} and remotes"))
-        parser.add_argument('-x', '--xrated', action='store_true', help=os.path.expandvars(f"Download video to {self.config.local['xdest']} and remotes"))
+        parser.add_argument('-v', '--keep-video', action='store_true', help=expandvars(f"Download video to {self.config.local['vdest']} and remotes"))
+        parser.add_argument('-x', '--xrated', action='store_true', help=expandvars(f"Download video to {self.config.local['xdest']} and remotes"))
         parser.add_argument('-V', '--video_dest', help=f"download video to the specified directory")
         return parser
--- src/dl.py
+++ src/dl.py
@@ -1,97 +1,100 @@
 #!/usr/bin/env python3
 
 import re
-import shutil
-import yt_dlp
-import sys
 from argument_parse import ArgParse
 from dl_config import DLConfig
 from media_file import MediaFile
-from remote import *
-from util import *
+from remote import Remote
+from util import run, samba_mount, samba_parse
 from os import environ
-from platform import uname
+from os.path import expandvars
+from shutil import copyfile
+from sys import abort
+from yt_dlp import YoutubeDL
 
-# See https://stackoverflow.com/a/64102901/553865
+class DL:
+    def __init__(self, args) -> None:
+        self.args = args
+        self.name = self.media_name()
 
-def media_name(URL):
-    with yt_dlp.YoutubeDL({'quiet': True}) as ydl:
-        info = ydl.extract_info(URL, download=False)
+    def media_name(self):
+        with YoutubeDL({'quiet': True}) as ydl:
+            info = ydl.extract_info(self.args.args.url, download=False)
 
-    name = re.sub(r'[^A-Za-z0-9 ]+', '', info['title']).strip().replace(' ', '_')
-    name = re.sub('__+', '_', name)
-    return name
+        name = re.sub(r'[^A-Za-z0-9 ]+', '', info['title']).strip().replace(' ', '_')
+        name = re.sub('__+', '_', name)
+        return name
 
+    def set_ydl_opts(self):
+        if args.action == 'video':
+            self.vdest = expandvars(config.local['vdest'])
+            if args.video_dest is not None:
+                self.vdest = args.video_dest
+            saved_filename = f"{self.vdest}/{self.name}"
+            self.ydl_opts = {
+                'format': 'mp4',
+                'outtmpl': f"{saved_filename}.mp4"
+            }
+        else:
+            saved_filename = expandvars(f"{config.local['mp3s']}/{self.name}")
+            self.ydl_opts = {
+                'outtmpl': saved_filename,
+                'postprocessors': [{  # Extract audio using ffmpeg
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': format,
+                }]
+            }
+        print(expandvars(f"Saving {saved_filename}.{format}"))
+        self.ydl_opts['quiet'] = True
 
-def doit(args):
-    global vdest
-    name = media_name(args.url)
+    def doit(self):
+        self.set_ydl_opts()
+        # See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
+        with yt_dlp.YoutubeDL(self.ydl_opts) as ydl:
+            # print(json.dumps(ydl.sanitize_info(info)))
+            error_code = ydl.download(args.args.url)
 
-    # See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
-    if action == 'video':
-        vdest = os.path.expandvars(config['local']['vdest'])
-        if args.video_dest is not None:
-            vdest = args.video_dest
-        saved_filename = f"{vdest}/{name}"
-        ydl_opts = {
-            'format': 'mp4',
-            'outtmpl': f"{saved_filename}.mp4"
-        }
-    else:
-        saved_filename = os.path.expandvars(f"{config['local']['mp3s']}/{name}")
-        ydl_opts = {
-            'outtmpl': saved_filename,
-            'postprocessors': [{  # Extract audio using ffmpeg
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': format,
-            }]
-        }
-    print(os.path.expandvars(f"Saving {saved_filename}.{format}"))
-    ydl_opts['quiet'] = True
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        # print(json.dumps(ydl.sanitize_info(info)))
-        error_code = ydl.download(args.url)
+        remotes = config['remotes']
+        if args.action == 'mp3':
+            mp3_name = self.name.replace('.webm', '.mp3')
+            mp3_name = mp3_name.replace('.mp4', '.mp3')
 
-    remotes = config['remotes']
-    if action == 'mp3':
-        mp3_name = name.replace('.webm', '.mp3')
-        mp3_name = mp3_name.replace('.mp4', '.mp3')
+            # run(f"mp3tag {mp3_name}")
+            for remote_name in list(remotes.keys()):
+                remote = remotes[remote_name]
+                if 'disabled' in remote and remote['disabled']:
+                    continue
 
-        # run(f"mp3tag {mp3_name}")
-        for remote_name in list(remotes.keys()):
-            remote = remotes[remote_name]
-            if 'disabled' in remote and remote['disabled']:
-                continue
+                method = remote['method'] if 'method' in remote and remote['method'] else 'scp'
+                mp3s = remote['mp3s']
+                source = f"{saved_filename}.{format}"
+                target = f"{remote_name}:{mp3s}"
+                if method == 'samba':
+                    remote_drive, local_path = samba_parse(remote_name, remote['mp3s'])
+                    samba_root = samba_mount(remote_name, remote_drive, args.debug)
+                    target = f"{samba_root}{local_path}/{mp3_name}.{format}"
+                    print(f"Copying to {target}")
+                    copyfile(source, target)
+                else:
+                    print(f"Copying to {target}/{mp3_name}.{format}")
+                    run(f"{method} {source} {target}", silent=not args.debug)
+        elif args.action == 'video':
+            for remote_name in list(remotes.keys()):
+                remote = remotes[remote_name]
+                if 'disabled' in remote and remote['disabled']:
+                    continue
 
-            method = remote['method'] if 'method' in remote and remote['method'] else 'scp'
-            mp3s = remote['mp3s']
-            source = f"{saved_filename}.{format}"
-            target = f"{remote_name}:{mp3s}"
-            if method == 'samba':
-                remote_drive, local_path = util.samba_parse(remote_name, remote['mp3s'])
-                samba_root = util.samba_mount(remote_name, remote_drive, args.debug)
-                target = f"{samba_root}{local_path}/{mp3_name}.{format}"
-                print(f"Copying to {target}")
-                shutil.copyfile(source, target)
-            else:
-                print(f"Copying to {target}/{mp3_name}.{format}")
-                util.run(f"{method} {source} {target}", silent=not args.debug)
-    elif action == 'video':
-        for remote_name in list(remotes.keys()):
-            remote = remotes[remote_name]
-            if 'disabled' in remote and remote['disabled']:
-                continue
-
-            method = remote['method'] if 'method' in remote and remote['method'] else 'scp'
-            dest = remote['vdest']
-            if args.xrated: dest = remote['xdest']
-            print(f"Copying {saved_filename}.{format} to {remote_name}:{dest}/{name}.{format}")
-            util.run(f"{method} {saved_filename}.{format} {remote_name}:{dest}", silent=not args.debug)
-    else:
-        sys.abort(f"Invalid action '{action}'")
+                method = remote['method'] if 'method' in remote and remote['method'] else 'scp'
+                dest = remote['vdest']
+                if args.xrated: dest = remote['xdest']
+                print(f"Copying {saved_filename}.{format} to {remote_name}:{dest}/{self.name}.{format}")
+                run(f"{method} {saved_filename}.{format} {remote_name}:{dest}", silent=not args.debug)
+        else:
+            abort(f"Invalid action '{args.action}'")
 
 if __name__ == '__main__':
     config = DLConfig()
     args = ArgParse(config)
-    media_file = MediaFile(config = config.local, path='')
-    doit(args)
+    # media_file = MediaFile(config = config.local, path='')
+    dl = DL(args)
+    dl.doit(args)
