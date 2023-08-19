7bd63b3ddee3ee2b25004526ac5069c6e3ce7751
Mike Slinn <mslinn@mslinn.com>
2023-08-19 10:48:48 -0400

    -

--- src/remote.py
+++ src/remote.py
@@ -2,19 +2,24 @@ import shutil
 import util
 
 class Remote:
-    def __init__(self, node_name, disabled=False, method='scp', mp3s = None, vdest = None, xdest = None) -> None:
+    def __init__(self, node_name, disabled=False, method='scp', mp3_path = None, video_path = None, x_path = None) -> None:
+       if not method in ['scp', 'samba']:
+            print(f"Error: method '{self.method}' is invalid. Allowable values are: samba and scp")
+            exit(1)
+
        self.disabled = disabled
        self.method = method
        self.node_name = node_name
-       self.mp3s = mp3s
-       self.vdest = vdest
-       self.xdest = xdest
+       self.mp3s = mp3_path
+       self.video_path = video_path
+       self.x_path = x_path
 
-    def copy_to(self, other: 'Remote', path):
+    # Copy source to remote
+    def copy_to(self, source, other: 'Remote'):
         if self.method == 'samba':
-            remote_drive, local_path = util.samba_parse(other.node_name, other.mp3s)
-            samba_root = util.samba_mount(other.node_name, path, self.debug)
-            target = f"{samba_root}{local_path}/{self.mp3_name}.{format}"
+            remote_drive, remote_path = util.samba_parse(other.node_name, other.mp3_path)
+            samba_root = util.samba_mount(other.node_name, remote_drive, self.debug)
+            target = f"{samba_root}{remote_path}/{self.mp3_name}.{format}"
             print(f"Copying to {target}")
             shutil.copyfile(source, target)
         else:
